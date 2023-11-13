81592a05feac9230372c973ee3b783e307c15534
ManoelPatrocinio <manoelpatrocinio99@gmail.com>
2023-11-12 22:43:06 -0300

    estrutura de pasta, pratica 1

---
+++ README.md
@@ -0,0 +1,12 @@
+## TRILHA DE PROGRAMAÇÃO BÁSICA EM PYTHON
+
+### MEMBROS DA EQUIPE:
+    Manoel Patrocinio | Branch : manoel | E-MAIL: manoelpatrocinio99@gmail.com
+
+
+## ESTRUTURA DE PASTAS
+    REPOSITÓRIO
+        SEMANA
+            ATV EM SALA
+            PRATICA
+        README.md
---
+++ Semana1/Pratica01/exercicio05.py
@@ -0,0 +1,16 @@
+print("EXPONENCIAÇÃO COM VARIAVEIS FLUTUANTES  EM PYTHON \n")
+print(2.0**9)
+
+
+var1 = 19.99        
+var2 = var1       
+
+print("VARIÁVEIS FLOATS IMUTÁVEIS EM PYTHON \n")
+
+print("\nExemplo Prático: \n")
+print("o valor de var1 = ", var1)
+print("o valor de var2 = ", var2)
+print("\nApós incrementar o valor de var1 em 1 (+= 1):\n")
+var1 += 1         # incrementando var1 em 1
+print("o valor de var1 = ", var1)
+print("o valor de var2 = ", var2)
