4e36c7e362c12cd6ee1ade3209609773e9314113
ManoelPatrocinio <manoelpatrocinio99@gmail.com>
2023-11-15 18:52:57 -0300

    atualizacao questao 2

--- Semana1/Pratica01/exercicio05.py
+++ Semana1/Pratica01/exercicio05.py
@@ -1,12 +1,17 @@
-print("EXPONENCIAÇÃO COM VARIAVEIS FLUTUANTES  EM PYTHON \n")
-print(2.0**9)
+import sys
 
+expo2 = 2.0
+print("EXPONENCIAÇÃO COM VARIAVEIS FLUTUANTES  EM PYTHON \n")
+print("Menor valor da  potencia de 2 : ", expo2 ** sys.float_info.min)
+print("Maior valor da  potencia de 2 : ", expo2 ** sys.float_info.max)
+
+
+   
+
+print("\nVARIÁVEIS FLOATS IMUTÁVEIS EM PYTHON \n")
 
 var1 = 19.99        
-var2 = var1       
-
-print("VARIÁVEIS FLOATS IMUTÁVEIS EM PYTHON \n")
-
+var2 = var1    
 print("\nExemplo Prático: \n")
 print("o valor de var1 = ", var1)
 print("o valor de var2 = ", var2)
