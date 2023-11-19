f87433efa52724bed97e3e87eb5f49b8176150ff
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-11-18 21:45:38 -0300

    Operadores Aritméticos Concluido

--- Semana1/Pratica01/exercicio02.py
+++ Semana1/Pratica01/exercicio02.py
@@ -14,3 +14,51 @@ C/C++;
 ● Verifique quais métodos estão disponíveis para as variáveis inteiras; 
 
 '''
+
+import os
+import platform
+
+def pause():
+      input("\tPressione Enter para continuar...")
+
+def limpaTela():
+    sistema_operacional = platform.system().lower()
+
+    if sistema_operacional == "windows":
+      os.system("cls")
+    elif sistema_operacional == "linux":
+      os.system("clear")
+    else:
+      print("Sistema operacional não suportado para limpar a tela.")
+
+limpaTela()      
+print('\n\t='+'=' *60)
+print('\t+ - * // % Operadores Aritméticos em Python + - * // %')
+
+# Adição
+a = 5
+b = 3
+print('\n\tAdição: {} + {} = {}'.format(a, b, a + b))
+
+# Subtração
+print('\tSubtração: {} - {} = {}'.format(a, b, a - b))
+
+# Multiplicação
+print('\tMultiplicação: {} x {} = {}'.format(a, b, a * b))
+
+# Divisão
+print('\tDivisão: {} / {} = {:.2f}'.format(a, b, a / b))
+
+# Exponenciação
+print('\tExponenciação: {} ^ {} = {}'.format(a, b, a**b))
+
+# Divisão inteira
+print('\tDivisão inteira: {} // {} = {}'.format(a, b, a // b))
+
+# Resto da divisão
+print('\tResto da divisão: {} % {} = {}'.format(a, b, a % b))
+
+print('\t='+'=' *60)
+pause()
+limpaTela()
+
