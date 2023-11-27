0732243df4f47d1a830c13c7b0de42ecb72cc208
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-11-26 21:26:39 -0300

    Menu Finalizado

--- Semana3/Pratica3/Exercicio2/GestaoFuncionario.py
+++ Semana3/Pratica3/Exercicio2/GestaoFuncionario.py
@@ -20,6 +20,33 @@ data_e_hora_atual = datetime.now()
 funcionarios = {}
 proximo_id = 1
 
+def menu():
+    
+  while True:
+    
+    limpaTela()
+    formato_personalizado = "\n\t%A, %d de %B de %Y %H:%M:%S"
+    data_e_hora_formatada = data_e_hora_atual.strftime(formato_personalizado)
+    print(data_e_hora_formatada)
+    print("\tFalta", (datetime(data_e_hora_atual.year, 12, 31) - data_e_hora_atual).days + 1, "dias para o fim do ano")
+    
+    print("\n\t======= GESTÃO DE FUNCIONÁRIO =======")
+    print("\t[1] - CADASTRAR")
+    print("\t[2] - LISTAR")
+    print("\t[3] - EDITAR")
+    print("\t[4] - EXCLUIR")
+    print("\t[5] - CONSULTAR")
+    print("\t[6] - REAJUSTE DE 10%")
+    print("\t[0] - SAIR")
+    opcao = input("\tENTRADA -> ")
+
+    if(opcao == "1" or opcao == "2" or opcao == "3" or opcao == "4" or opcao == "5" or opcao == "6" or opcao == "0"):
+      return opcao
+    else:
+      limpaTela()
+      print("\n\tOps, opção inválida! Tente novamente.")
+      pause()
+
 def pause():
   input("\tPressione Enter para continuar...")
   
