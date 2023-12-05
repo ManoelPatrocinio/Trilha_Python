b449aad4dc59b566c483d0c80c860102691eb861
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-12-05 11:19:13 -0300

    Atividade em Equipe Finalizado

--- Semana4/pratica4/emGrupo/DataFruta.py
+++ Semana4/pratica4/emGrupo/DataFruta.py
@@ -734,10 +734,51 @@ def menu():
             print("\n\tOps, opção inválida! Tente novamente.")
             pause()
 
-
+def aplicativo():
+    
+    nomes = ListaNomes()
+    salarios = ListaSalarios()
+    datas = ListaDatas()
+    idades = ListaIdades()
+    
+    while True:
+        
+        opcao = menu()
+        
+        match opcao:
+           
+            case "1":
+                nomes.entradaDeDados()  
+                nomes.listarEmOrdem()
+            case "2":
+                salarios.entradaDeDados()
+                salarios.listarEmOrdem()    
+            case "3":
+                datas.entradaDeDados()
+                datas.listarEmOrdem()
+            case "4":
+                idades.entradaDeDados()
+                idades.listarEmOrdem()
+                pass   
+            case "5":
+                nomes.percorreListaDeNomesESalarios(nomes, salarios)
+            case "6":
+                salarios.reajustar_Salarios()
+            case "7":
+                datas.modificar_datas_anteriores_2019() 
+            case "0":
+                limpaTela()
+                print("\n\tObrigado por usar o DataFruta!")
+                pause()
+                exit()  
+            case _:
+                limpaTela()
+                print("\n\tOps, opção inválida! Tente novamente.")
+                pause()
+    pass
               
 def main():
-   
+    aplicativo()
     
 if __name__ == "__main__":
     main()
