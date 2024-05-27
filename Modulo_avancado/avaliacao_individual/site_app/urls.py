79621963db9c9cb88c43af2fe2bcb648baa008f4
ManoelPatrocinio <manoelpatrocinio99@gmail.com>
2024-05-27 01:12:41 -0300

    organização de pastas

--- Modulo_avancado/avaliacao_individual/site_app/urls.py
+++ Modulo_avancado/avaliacao_individual/site_app/urls.py
@@ -1,15 +1,12 @@
 
 # from django.contrib import admin
 from django.urls import path
-from api_prova_app import views
+from site_app import views
 
 urlpatterns = [
     path('produtos', views.produtos),
     path('produto/<produto_id>', views.produto_id),
-    
-    path('relacoes', views.relacoes),
-    path('relacao_id/<relacao_id>', views.relacao_id),
-    
+
       
 ]
  
